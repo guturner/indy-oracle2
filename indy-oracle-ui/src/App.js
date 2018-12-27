3e1b3ca4097a961f15774eece73a1af426e68983
Guy Turner <guturner@users.noreply.github.com>
2018-12-27 16:55:16 -0500

    Adding indy-oracle-ui

---
+++ indy-oracle-ui/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="green">
+            ~ $ indy_oracle init <strong className="blinking">|</strong>
+          </h1>
+        </header>
+      </div>
+    );
+  }
+  
+  
+}
+
+export default App;
