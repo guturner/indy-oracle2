90c69fccc077a8034bc816bf4a715cf66ee1b261
Guy Turner <guy.turner@hotmail.com>
2019-01-01 21:05:55 -0500

    Proving k8s service works.

--- indy-oracle-api/src/main/java/com/indyoracle/api/controllers/UserController.java
+++ indy-oracle-api/src/main/java/com/indyoracle/api/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.indyoracle.api.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @GetMapping("/1.0/user")
+    @CrossOrigin(origins = "http://localhost:3000")
     public String sayHello() {
         return "Hello!";
     }
--- indy-oracle-ui/src/views/LandingPage/Sections/InfoSection.jsx
+++ indy-oracle-ui/src/views/LandingPage/Sections/InfoSection.jsx
@@ -10,23 +10,41 @@ import GridItem from "components/Grid/GridItem.jsx";
 import CustomInput from "components/CustomInput/CustomInput.jsx";
 import Button from "components/CustomButtons/Button.jsx";
 
+import axios from 'axios';
+
 import infoStyle from "assets/jss/material-kit-react/views/landingPageSections/infoStyle.jsx";
 
 class InfoSection extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      msg: 'Loading...'
+    };
+  }
+
   render() {
     const { classes } = this.props;
+
     return (
       <div className={classes.section}>
         <GridContainer justify="center">
           <GridItem cs={12} sm={12} md={8}>
             <h2 className={classes.title}>More Info</h2>
             <h4 className={classes.description}>
+              {this.state.msg}
+              <br/>
               Coming soon.
             </h4>
           </GridItem>
         </GridContainer>
       </div>
     );
+  }
+
+  componentWillMount() {
+    axios.get('indy-oracle-api-svc:8080/api/1.0/user')
+        .then(response => this.setState({msg: response.data}));
   }
 }
 
