1f0a76dd65872b7658c3d309a28341321140a4fd
Guy Turner <guturner@users.noreply.github.com>
2019-01-05 20:10:53 -0500

    Automatic HTTPS redirect.

--- indy-oracle-ui/build.bat
+++ indy-oracle-ui/build.bat
@@ -1,2 +1,2 @@
-docker build -t gcr.io/third-anvil-135423/indy-oracle-ui:1.3.2 .
-docker push gcr.io/third-anvil-135423/indy-oracle-ui:1.3.2
+docker build -t gcr.io/third-anvil-135423/indy-oracle-ui:1.3.3 .
+docker push gcr.io/third-anvil-135423/indy-oracle-ui:1.3.3
