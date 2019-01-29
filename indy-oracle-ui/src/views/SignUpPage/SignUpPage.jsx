693cbdb3d4952109d07b8ac38fcdab171219607a
Guy Turner <guy.turner@hotmail.com>
2019-01-28 20:19:19 -0500

    Building SignUpPage component.

--- indy-oracle-ui/src/views/SignUpPage/SignUpPage.jsx
+++ indy-oracle-ui/src/views/SignUpPage/SignUpPage.jsx
@@ -1,4 +1,14 @@
 import React from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
+
+import Button from "components/CustomButtons/Button.jsx";
+import Card from "components/Card/Card.jsx";
+import CardBody from "components/Card/CardBody.jsx";
+import CustomInput from "components/CustomInput/CustomInput.jsx";
+import GridContainer from "components/Grid/GridContainer.jsx";
+import GridItem from "components/Grid/GridItem.jsx";
+
+import basicsStyle from "assets/jss/material-kit-react/views/componentsSections/basicsStyle.jsx";
 
 class SignUpPage extends React.Component {
 
@@ -38,37 +48,75 @@ class SignUpPage extends React.Component {
       this.state.password === '';
 
     return (
-      <form onSubmit={this.onSubmit}>
-        
-        <input
-          name="email"
-          value={this.state.email}
-          onChange={this.onChange}
-          type="text"
-          placeholder="Email Address"
-        />
+      <Card>
+        <CardBody>
+          <form onSubmit={this.onSubmit}>
 
-        <input
-          name="password"
-          value={this.state.password}
-          onChange={this.onChange}
-          type="password"
-          placeholder="Password"
-        />
+            <GridContainer>
 
-        <input
-          name="confirmPassword"
-          value={this.state.confirmPassword}
-          onChange={this.onChange}
-          type="password"
-          placeholder="Confirm Password"
-        />
+              <GridItem xs={8} sm={8} md={8}>
+                <CustomInput
+                  labelText="Email Address"
+                  formControlProps={{
+                    fullWidth: true
+                  }}
+                  inputProps={{
+                    name: "email",
+                    value: this.state.email,
+                    onChange: this.onChange
+                  }}
+                />
+              </GridItem>
 
-        <button type="submit" disabled={isInvalid}>Sign Up</button>
+            </GridContainer>
 
-      </form>
+            <GridContainer>
+
+              <GridItem xs={4} sm={4} md={4}>
+                <CustomInput
+                  labelText="Password"
+                  formControlProps={{
+                    fullWidth: true
+                  }}
+                  inputProps={{
+                    name: "password",
+                    type: "password",
+                    value: this.state.password,
+                    onChange: this.onChange
+                  }}
+                />
+              </GridItem>
+
+              <GridItem xs={4} sm={4} md={4}>
+                <CustomInput
+                  labelText="Confirm Password"
+                  formControlProps={{
+                    fullWidth: true
+                  }}
+                  inputProps={{
+                    name: "confirmPassword",
+                    type: "password",
+                    value: this.state.confirmPassword,
+                    onChange: this.onChange
+                  }}
+                />
+              </GridItem>
+            
+            </GridContainer>
+
+            <GridContainer>
+
+              <GridItem xs={4} sm={4} md={4}>
+                <Button type="submit" disabled={isInvalid}>Sign Up</Button>
+              </GridItem>
+            
+            </GridContainer>
+
+          </form>
+        </CardBody>
+      </Card>
     );
   }
 }
 
-export default SignUpPage;
+export default withStyles(basicsStyle)(SignUpPage);
