f923cff700d8f8d1dbd570c4ac9e0692233a4510
Guy Turner <guturner@users.noreply.github.com>
2019-02-08 22:09:02 -0500

    FEAT-10: Different SMS response if sender is not a user.

--- indy-oracle-api/src/main/java/com/indyoracle/api/controllers/EmailController.java
+++ indy-oracle-api/src/main/java/com/indyoracle/api/controllers/EmailController.java
@@ -1,39 +1,72 @@
 package com.indyoracle.api.controllers;
 
+import com.indyoracle.api.config.TwilioConfigProperties;
+import com.indyoracle.api.dtos.User;
+import com.indyoracle.api.services.UserService;
 import com.twilio.twiml.MessagingResponse;
 import com.twilio.twiml.messaging.Body;
 import com.twilio.twiml.messaging.Message;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.http.HttpStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 public class EmailController {
+    private final Logger LOGGER = LoggerFactory.getLogger(EmailController.class);
+
+    private final TwilioConfigProperties twilioConfigProperties;
+    private final UserService userService;
+
+    @Autowired
+    public EmailController(
+            TwilioConfigProperties twilioConfigProperties,
+            UserService userService) {
+        this.twilioConfigProperties = twilioConfigProperties;
+        this.userService = userService;
+    }
 
     @PostMapping(value = "/1.0/sms/", produces = MediaType.APPLICATION_XML_VALUE)
     @ApiOperation("Twilio WebHook.")
-    public ResponseEntity<String> twilioWebHook(@RequestParam("From") String from, @RequestParam("Body") String requestBody) {
-        if (!StringUtils.isEmpty(from)) {
+    public ResponseEntity<String> twilioWebHook(
+            @RequestParam("From") String from,
+            @RequestParam("Body") String requestBody,
+            @RequestParam("AccountSid") String accountSid) {
+        if (!twilioConfigProperties.getSid().equals(accountSid)) {
+            LOGGER.warn("Unknown Entity: {} attempted to post Body: {}.", from, requestBody);
+            return ResponseEntity.badRequest().body("Naughty, naughty... The Oracle is watching.");
+        }
 
-            Body body = new Body
+        Optional<User> user = userService.getUsers().stream()
+                .filter(u -> u.getPhoneNumber().equals(from))
+                .findFirst();
+
+        Body body;
+        if (user.isPresent()) {
+            body = new Body
                     .Builder("Indy Oracle is online.")
                     .build();
-            Message sms = new Message
-                    .Builder()
-                    .body(body)
-                    .build();
-            MessagingResponse twiml = new MessagingResponse
-                    .Builder()
-                    .message(sms)
-                    .build();
-            return ResponseEntity.ok(twiml.toXml());
         } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            body = new Body
+                    .Builder("Hello, citizen!\nIndy Oracle is expanding. Find out more at https://indy-oracle.com/\nIf this is an emergency, please dial 911.")
+                    .build();
         }
+
+        Message sms = new Message
+                .Builder()
+                .body(body)
+                .build();
+        MessagingResponse twiml = new MessagingResponse
+                .Builder()
+                .message(sms)
+                .build();
+        return ResponseEntity.ok(twiml.toXml());
     }
 }
